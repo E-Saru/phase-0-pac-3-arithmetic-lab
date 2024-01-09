17a40642c3aba9c792187151cb015dc28659cffe
E.Saru <efjeniah.mwawughanga@student.moringaschool.com>
2024-01-09 11:18:31 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+var numbers = a, b;
+
+function add(a, b) {
+  return (a+b);
+}
+function subtract(b, a){
+  return (b-a);
+}
+function multiply(a, b) {
+  return (a*b);
+}
+function divide(a, b) {
+  return (a/b);
+}
+function increment(a){
+  return (a+1);
+}
+function decrement(a){
+  return (a-1);
+}
+var a="0*2328";
+function makeInt(a){
+  return (parseInt(a, 10));
+}
+var n='2.222';
+function preserveDecimal(n){
+  return (parseFloat(n));
+}
